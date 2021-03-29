71f41c2007e8ee08af0a1fee6b3c52b8b20f56e9
khanusmanr <67205344+khanusmanr@users.noreply.github.com>
2021-03-29 18:44:10 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace optoNCDTSim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "Book 1.csv";
+            StreamReader reader = null; 
+            
+            if(File.Exists(filePath))
+            {
+                reader = new StreamReader(File.OpenRead(filePath));
+                List<string> listA = new List<string>(); 
+                while(!reader.EndOfStream){
+                    var line = reader.ReadLine(); 
+                    var values = line.Split(',');
+                    if(line == null)
+                    { 
+                        values.RemoveEmptyEntries; 
+                    }
+                    foreach(var item in values)
+                    { 
+                        listA.Add(item); 
+                    }
+                    foreach (var coloumn1 in listA)
+                    { 
+                        Console.WriteLine(coloumn1); 
+                    }
+                }
+
+            } 
+            else{ 
+                Console.WriteLine("File doesn't exist"); 
+            }
+            Console.ReadLine(); 
+        }
+    }
+}
